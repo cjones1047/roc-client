9975e92cd2ffdd2c4294b0db9e8ca3f84aba68da
cjones1047 <cjmj1047@gmail.com>
2023-08-12 08:49:22 -0500

    updated code for App.tsx and index.tsx to use MUI as default styling library; created and updated theme.tsx file to hold custom MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, AppBar, Toolbar, Typography, Container } from '@mui/material';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My React App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Typography variant="body1" gutterBottom>
+          Welcome to the React App with MUI and TypeScript.
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2', // You can customize this
+        },
+        secondary: {
+            main: '#ff9800', // And this too
+        },
+    },
+});
+
+export default theme;
